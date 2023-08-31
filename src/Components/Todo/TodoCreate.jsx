645c07d0fe7522e694dc5fba1d2b51e863fee9a7
Chinnawat-TT <Chinnawat.sangrung@gmail.com>
2023-08-31 11:04:21 +0700

    condition render todofrom to add

--- src/Components/Todo/TodoCreate.jsx
+++ src/Components/Todo/TodoCreate.jsx
@@ -1,15 +1,73 @@
-import styles from './TodoCreate.module.scss';
-import { FaPlus } from 'react-icons/fa';
-import { HiPlus } from 'react-icons/hi';
+import { useState } from "react";
+import styles from "./TodoCreate.module.scss";
+import TodoForm from "./TodoForm";
+import { FaPlus } from "react-icons/fa";
+import { HiPlus } from "react-icons/hi";
+/* 
+#CC1 condition rendering
+- Default : show button & text
+- Active : show TodoForm
+concept : ture ? <AddTask/> :<TodoForm/>  => render <Addtask/>
+concept : false ? <AddTask/> :<TodoForm/>  => render <TodoForm/>
+*/
 
+/*
+#CC2 event handling
+  - เอาฟังก์ชัั่นไปผูกติดกับ UI เพื่อให้  USER  เป็นคนเรียกฟังก์ชั่นเอง
+  - onClick : ต้อง  click ก่อน, Fn ถึงจะรัน
+    - User  ทำการคลิก
+    - Browser จะเป็นคนเรียกใช้ฟังก์ชั่น โดยส่ง parameter มา 1 ตัว handleClick(evenObject)
+
+ */
+
+/*
+#CC3 JS value ไม่สามารถทำให้ React Rerender ได้
+ต้องใช้  state
+*/
+
+/*
+#CC4 Array Destruring 
+function myUseState(){
+  return [5,9]
+}
+let [a,b] = myUseState()
+a= 5
+b= 9
+
+*/
+
+/*
+#CC5 React State  (1 ในฟังก์ชั่นของกลุ่ม  React Hook)
+const [ state,setState ] = useState(initiaState:any)
+*/
+
+// #1 : FC = Function Component (Render)
 function TodoCreate() {
+  // Hook FN
+  const [ isOpenForm,setIsOpenForm ]=useState(false)
+
+  // #2 :  JS Function (Logic)
+  const handleClick = function (event) {
+    setIsOpenForm(!isOpenForm)
+    console.log("clicked")
+
+  };
+
   return (
-    <div className={styles.todo__create}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
-      </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+    <>
+      {isOpenForm ? (
+       <TodoForm />
+      ) : (<div 
+        className={styles.todo__create}
+        onClick={handleClick}>
+          <div className={styles.todo__create__button}>
+            <HiPlus />
+          </div>
+          <h3 className={styles.todo__create__text}>Add Task</h3>
+        </div>
+       
+      )}
+    </>
   );
 }
 
